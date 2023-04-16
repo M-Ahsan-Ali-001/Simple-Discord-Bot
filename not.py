019abc5d84f391c52d057bdc15be622f50bdce8c
Muhammad Ahsan Ali <91987110+M-Ahsan-Ali-001@users.noreply.github.com>
2023-04-17 00:08:17 +0500

    Create not.py

---
+++ not.py
@@ -0,0 +1,23 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Logged in as', client.user.name)
+    print('Bot ID:', client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run('Token')
